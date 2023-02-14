b7a606d3f708c3f053e4e88dd54afe1fe4d812b1
Anton Keks <anton@codeborne.com>
2023-02-14 14:29:34 +0200

    add more sample fields

--- ui/src/pages/ComponentsDemo.svelte
+++ ui/src/pages/ComponentsDemo.svelte
@@ -2,11 +2,34 @@
   import {_} from 'src/i18n'
   import MainPageLayout from 'src/layout/MainPageLayout.svelte'
   import UserForm from 'src/pages/UserForm.svelte'
+  import UnitsField from 'src/forms/UnitsField.svelte'
+  import AmountField from 'src/forms/AmountField.svelte'
+  import NumberField from 'src/forms/NumberField.svelte'
+  import RangeSlider from 'src/forms/RangeSlider.svelte'
+  import TextAreaField from 'src/forms/TextAreaField.svelte'
+  import SelectField from 'src/forms/SelectField.svelte'
+  import MultipleSelect from 'src/forms/MultipleSelect.svelte'
+  import Form from 'src/forms/Form.svelte'
+
+  let range = 33
 </script>
 
 <MainPageLayout title={_('title')}>
   <div class="common-grid">
     <h2>Sample user form</h2>
     <UserForm/>
+
+    <h2>More advanced components</h2>
+    <Form submit={() => alert('Nowhere to submit')}>
+      <TextAreaField label="TextArea" rows={3} value="Here we have char counter"/>
+      <div class="common-grid sm:grid-cols-2">
+        <NumberField label="NumberField" unit="%" bind:value={range}/>
+        <AmountField label="AmountField"/>
+        <RangeSlider label="RangeSlider" min={10} max={40} bind:value={range}/>
+        <UnitsField label="UnitsField" value={100} units={['€ excl. VAT', '€ incl. VAT']} unitRatio={1.2}/>
+        <SelectField label="SelectField" options={{1: 'a', 2: 'b', 5: 'c'}} emptyOption="Please select something"/>
+        <MultipleSelect label="MultipleSelect" options={{1: 'a', 2: 'b', 5: 'c'}} values={[1, 5]}/>
+      </div>
+    </Form>
   </div>
 </MainPageLayout>
