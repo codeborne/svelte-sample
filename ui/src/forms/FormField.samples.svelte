4533e5060a56fea7094a2452d3aad84f03be6650
Anton Keks <anton@codeborne.com>
2023-02-15 14:23:06 +0200

    add helpText sample

--- ui/src/forms/FormField.samples.svelte
+++ ui/src/forms/FormField.samples.svelte
@@ -9,6 +9,7 @@
   <FormField label="type=email" type="email"/>
   <FormField label="Disabled" disabled/>
   <FormField label="Readonly" readonly/>
+  <FormField label="With help" helpText="Mega additional help"/>
 
   <div class="validated">
     <FormField label="Validated"/>
