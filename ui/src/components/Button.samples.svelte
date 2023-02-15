4d0767d183c830ef706c9d248652ff354cfaa1ae
Anton Keks <anton@codeborne.com>
2023-02-15 14:11:31 +0200

    iterate over sizes

--- ui/src/components/Button.samples.svelte
+++ ui/src/components/Button.samples.svelte
@@ -1,34 +1,17 @@
 <script lang="ts">
   import Button from 'src/components/Button.svelte'
 
-  const def = {label: 'Default'}
-  const props = [
-    def,
-    {...def, size: 'lg'},
-    {...def, size: 'sm'}
-  ]
+  const sizes = ['sm', '', 'lg']
 </script>
 
 <div class="common-grid sm:grid-cols-4">
-  <div class="flex flex-col gap-6">
-    {#each props as p}
-      <Button {...p}/>
-    {/each}
-  </div>
-
-  <div class="flex flex-col gap-6">
-    <Button label="Primary" class="primary"/>
-    <Button label="Primary lg" class="primary" size="lg"/>
-    <Button label="Primary sm" class="primary" size="sm"/>
-  </div>
-  <div class="flex flex-col gap-6">
-    <Button label="Danger" class="danger"/>
-    <Button label="Danger lg" class="danger" size="lg"/>
-    <Button label="Danger sm" class="danger" size="sm"/>
-  </div>
-  <div class="flex flex-col gap-6">
-    <Button label="Success" class="success"/>
-    <Button label="Success lg" class="success" size="lg"/>
-    <Button label="Success sm" class="success" size="sm"/>
-  </div>
+  {#each sizes as size}
+    <div class="flex flex-col gap-6">
+      <h2>Size {size || 'default'}</h2>
+      <Button label="Default" {size}/>
+      <Button label="Primary" class="primary" {size}/>
+      <Button label="Danger" class="danger" {size}/>
+      <Button label="Success" class="success" {size}/>
+    </div>
+  {/each}
 </div>
