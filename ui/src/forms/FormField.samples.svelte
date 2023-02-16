ee2c572d6dae7feb90427934144995fe55df426a
Anton Keks <anton@codeborne.com>
2023-02-16 15:41:18 +0200

    add type samples separately

--- ui/src/forms/FormField.samples.svelte
+++ ui/src/forms/FormField.samples.svelte
@@ -3,14 +3,14 @@
   import FormField from 'src/forms/FormField.svelte'
   import Sample from 'src/samples/Sample.svelte'
   import UserForm from 'src/samples/UserForm.svelte'
+  import {today} from 'src/i18n'
 </script>
 
-<Sample title="Common states">
-  <div class="w-64 mx-auto">
+<Sample title="States & Types">
+  <div class="common-grid sm:grid-cols-2">
     <Form submit={() => alert('No submit')}>
       <FormField label="FormField"/>
       <FormField label="FormField" placeholder="Placeholder"/>
-      <FormField label="type=email" type="email"/>
       <FormField label="Disabled" disabled/>
       <FormField label="Readonly" readonly/>
       <FormField label="With help" helpText="Mega additional help"/>
@@ -18,6 +18,14 @@
       <div class="validated">
         <FormField label="Validated"/>
       </div>
+    </Form>
+
+    <Form submit={() => alert('No submit')}>
+      <FormField label="type=email" type="email"/>
+      <FormField label="type=date, max=today" type="date" max={today}/>
+      <FormField label="type=time" type="time"/>
+      <FormField label="type=datetime" type="datetime"/>
+      <FormField label="type=search" type="search"/>
     </Form>
   </div>
 </Sample>
