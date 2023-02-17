d8c5169ae04c3d37e92b7e5bac4231fed459e1b6
Anton Keks <anton@codeborne.com>
2023-02-17 11:55:08 +0200

    add modal samples

---
+++ ui/src/components/Modal.samples.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Modal from 'src/components/Modal.svelte'
+  import UserForm from 'src/samples/UserForm.svelte'
+  import Button from 'src/components/Button.svelte'
+
+  let showNormal = false, showWide = false
+</script>
+
+<Button label="Show Normal Modal" on:click={() => showNormal = true}/>
+<Button label="Show Wide Modal" on:click={() => showWide = true}/>
+
+<Modal title="Normal Modal" bind:show={showNormal}>
+  <UserForm/>
+</Modal>
+
+<Modal title="Wide Modal" bind:show={showWide} wide>
+  <UserForm/>
+</Modal>
