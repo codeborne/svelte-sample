ca701ed55e93f75848bf63a72e98841ceffc12bd
Anton Keks <anton@codeborne.com>
2023-04-06 13:08:02 +0300

    add some samples

--- ui/src/forms/Form.samples.svelte
+++ ui/src/forms/Form.samples.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
   import UserForm from 'src/samples/UserForm.svelte'
+  import Form from 'src/forms/Form.svelte'
+  import FormField from 'src/forms/FormField.svelte'
 </script>
 
 <p>Always wrap forms into a Form component for validation to work</p>
 <UserForm/>
+
+<p>Pre-validated</p>
+<Form submit={() => alert('TODO')} validated>
+  <FormField label="Invalid"/>
+</Form>
---
+++ ui/src/forms/PhoneField.samples.svelte
@@ -0,0 +1,9 @@
+<script>
+  import PhoneField from 'src/forms/PhoneField.svelte'
+  import {CountryCode} from 'src/api/types'
+</script>
+
+<div class="w-64 common-grid">
+  <PhoneField label="With known country" countryCode={CountryCode.EE}/>
+  <PhoneField label="With unknown country"/>
+</div>
