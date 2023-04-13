cdb0794eff386ff8f138c56abbf3139db6675425
Anton Keks, Vadim Vohmjanin <anton@codeborne.com, vadim@codeborne.com>
2023-04-13 12:17:46 +0300

    do not show "-" before any address is found

--- ui/src/forms/EstonianAddressSearch.samples.svelte
+++ ui/src/forms/EstonianAddressSearch.samples.svelte
@@ -1,21 +1,19 @@
-<script>
+<script lang="ts">
   import EstonianAddressSearch from 'src/forms/EstonianAddressSearch.svelte'
   import CheckboxField from 'src/forms/CheckboxField.svelte'
+  import type {EstonianAddress} from 'src/api/estonianAddressRegister'
 
-  let address
+  let address: EstonianAddress
   let showApartments = true
   let searchApartments = true
   let showNonAddressObjects = false
 </script>
+
 <CheckboxField bind:checked={showApartments} label="Select apartments"/>
 <CheckboxField bind:checked={searchApartments} label="Search for apartments"/>
 <CheckboxField bind:checked={showNonAddressObjects} label="Show non address objects"/>
 <EstonianAddressSearch bind:address={address} {showApartments} {searchApartments} {showNonAddressObjects}/>
-<div>
-  <span>{address?.adr_id || ''}</span> - <span>{address?.ipikkaadress || ''}</span>
-</div>
 
-
-
-
-
+{#if address}
+  <div>{address.adr_id || ''} - {address.ipikkaadress || ''}</div>
+{/if}
