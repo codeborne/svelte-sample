fe33049068a2d5872387fa137c81f8eb93f26cef
Anton Keks <anton@codeborne.com>
2023-07-17 11:26:39 +0300

    e.reason can be undefined in Firefox at least

--- ui/src/errorHandlers.ts
+++ ui/src/errorHandlers.ts
@@ -4,7 +4,7 @@ import {showToast, ToastType} from './stores/toasts'
 
 export function jsErrorHandler(message: Event|string, source?: string, line?: number, column?: number, error?: Error) {
   reportError({message, source, line, column}, error)
-  alert('Technical error occurred, please reload the page:\n' + message)
+  alert((error?.name == 'SyntaxError' ? 'Your browser is probably too old, please update:' : 'Technical error occurred, please reload the page:') + '\n' + message)
 }
 
 export function reportError(body: object, error?: Error) {
@@ -15,17 +15,15 @@ export function reportError(body: object, error?: Error) {
   }).catch(e => console.error(e))
 }
 
-export function handleUnhandledRejection(e: PromiseRejectionEvent) {
+export function handleUnhandledRejection(event: PromiseRejectionEvent) {
+  const e: Error & {statusCode: number} | undefined = event.reason
   console.error(e)
-  if (e.reason?.stack) {
-    jsErrorHandler(e.reason.message, undefined, undefined, undefined, e.reason)
-    return
-  }
-  let error = e.reason?.message
-  if (error) error = _(e.reason?.message)
-  else error = _('errors.technical') + ': ' + e.reason
-  showToast(error, {type: ToastType.ERROR})
-  if (e.reason.statusCode === 401 || e.reason.statusCode === 403) setTimeout(() => navigate(''), 1000)
+  if (e?.stack) return jsErrorHandler(e.message, undefined, undefined, undefined, e)
+  let message = e?.message
+  if (message) message = _(message)
+  else message = _('errors.technical') + ': ' + e
+  showToast(message, {type: ToastType.ERROR})
+  if (e?.statusCode === 401 || e?.statusCode === 403) setTimeout(() => navigate(''), 1000)
 }
 
 export function initErrorHandlers() {
