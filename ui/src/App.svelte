99ca3dea0e80835f5fc1869476a521dec7b9eec8
Vadim Vohmjanin <vadim@codeborne.com>
2023-08-18 17:02:44 +0300

    fix imports and move tests

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,9 +1,13 @@
+<script context="module">
+  import routes from '../.routify/routes.default.js'
+  import {createRouter, Router} from '@roxi/routify'
+
+  const router = createRouter({routes})
+</script>
+
 <script lang="ts">
   import {_} from 'src/i18n'
   import Toasts from './components/Toasts.svelte'
-  import {createRouter, Router} from '@roxi/routify'
-  import routes from '../.routify/routes.default.js'
-
 </script>
 
 <svelte:head>
@@ -12,8 +16,4 @@
 
 <Toasts/>
 
-<script context="module">
-  const router = createRouter({ routes })
-</script>
-
-<Router {router} />
+<Router {router}/>
