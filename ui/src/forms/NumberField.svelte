857face5249e4b6de717f1f09deec958ec8dce05
Anton Keks <anton@codeborne.com>
2023-08-25 11:30:47 +0300

    restore shorter maxlength for NumberField

--- ui/src/forms/NumberField.svelte
+++ ui/src/forms/NumberField.svelte
@@ -5,11 +5,12 @@
   export let value: string | number = ''
   export let unit = ''
   export let min = 0
+  export let maxlength = 10
 
   interface $$Events {input: FormEvent; change: FormEvent}
 </script>
 
-<FormField {label} type="number" {min} bind:value style="padding-right: {unit.length / 2 + 1.5}em" on:input on:change {...$$restProps}>
+<FormField {label} type="number" {min} {maxlength} bind:value style="padding-right: {unit.length / 2 + 1.5}em" on:input on:change {...$$restProps}>
   <div slot="after" class="absolute inset-y-0 right-3 flex items-center pointer-events-none">
     <span class="unit text-gray-500 sm:text-sm">{unit}</span>
   </div>
