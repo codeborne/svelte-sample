7b8458fbf68cb143ea6352bf96474ab780ee7da5
Erkki Teedla <erkki@codeborne.com>
2024-01-17 15:49:42 +0200

    add a prototype only component that can be used for mocking different scenarios / states

---
+++ ui/src/prototyping/MockingMenu.samples.svelte
@@ -0,0 +1,26 @@
+<script>
+  import MockingMenu from 'src/prototyping/MockingMenu.svelte'
+  import Button from 'src/components/Button.svelte'
+
+  let viewScope = 'future'
+</script>
+
+Check the menu at the edge of the page ↘️
+
+<MockingMenu title="Scenarios">
+  <div class="grid gap-2 text-xs">
+    <a href={'#'}>Some mock action</a>
+    <a href={'#'}>Another mock action</a>
+  </div>
+</MockingMenu>
+
+<MockingMenu position="bottom-right" icon="">
+  <div class="flex gap-1">
+    {#each [['mvp', 'MVP'], ['future', 'Future']] as [slug, title]}
+      <Button class="xs {viewScope === slug ? 'primary' : '' }"
+              on:click={() => viewScope = slug}>
+        {title}
+      </Button>
+    {/each}
+  </div>
+</MockingMenu>
---
+++ ui/src/prototyping/MockingMenu.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+
+  import Icon from 'src/icons/Icon.svelte'
+
+  export let position: 'right' | 'bottom-right' = 'right'
+  export let title = ''
+
+  export let icon = 'settings-automation'
+</script>
+
+<div class="fixed z-50 transition ease-in-out
+          opacity-60 hover:opacity-100
+          {position}
+">
+  <div class="grid gap-2 p-3 rounded-l shadow-lg bg-white">
+    {#if icon}
+      <div>
+        <Icon name={icon} class="text-muted"/>
+      </div>
+    {/if}
+    {#if title}
+      <strong>{title}</strong>
+    {/if}
+    <slot/>
+  </div>
+</div>
+
+<style>
+  .right {
+    @apply right-0 top-1/2 my-6 translate-x-3/4 hover:translate-x-0 w-[200px];
+  }
+
+  .right > div {
+    @apply -translate-y-1/2
+  }
+
+  .bottom-right {
+    @apply right-0 bottom-0 mx-6;
+  }
+</style>
