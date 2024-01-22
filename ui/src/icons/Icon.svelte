9cd3f8d6607234b66f48d5ea495642f7ad2d5d1c
Anna Tomberg <anna@codeborne.com>
2024-01-22 15:59:40 +0200

    Added shink-0 so that icon would not be shrinked down when its parent has flex settings

--- ui/src/icons/Icon.svelte
+++ ui/src/icons/Icon.svelte
@@ -6,7 +6,7 @@
   export let strokeWidth: string = '2'
 </script>
 
-{@html icons[name].replace('<svg', `<svg class="icon ${name} ${size} ${$$props.class ?? ''}" stroke-width=${strokeWidth} focusable="false"`)}
+{@html icons[name].replace('<svg', `<svg class="icon shrink-0 ${name} ${size} ${$$props.class ?? ''}" stroke-width=${strokeWidth} focusable="false"`)}
 
 <style global>
   .icon {
