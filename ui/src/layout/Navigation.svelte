162bf15c465da5755dd9cf8d8a32e55d49c436d6
Anton Keks <anton@codeborne.com>
2024-04-19 13:49:20 +0300

    fix label usage

--- ui/src/layout/Navigation.svelte
+++ ui/src/layout/Navigation.svelte
@@ -2,11 +2,11 @@
   import {Role} from 'src/api/types'
   import {user} from 'src/stores/auth'
   import Link from 'src/components/Link.svelte'
+  import {t} from 'src/i18n'
 
   const menu = [
-    {path: '', label: 'home.title', roles: [Role.ADMIN, Role.TRAINER, Role.CUSTOMER]}
+    {path: '', label: t.home.title, roles: [Role.ADMIN, Role.TRAINER, Role.CUSTOMER]}
   ]
-
 </script>
 
 {#each menu.filter(m => m.roles.includes($user?.role)) as m}
