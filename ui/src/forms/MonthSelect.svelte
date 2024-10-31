7129aa6e29debc960e096bfade04b46c58e39d9c
Anton Keks <anton@codeborne.com>
2024-10-31 10:43:08 +0200

    fix choice of September when today is Oct 31st

--- ui/src/forms/MonthSelect.svelte
+++ ui/src/forms/MonthSelect.svelte
@@ -3,20 +3,21 @@
   import {lang} from '../i18n'
 
   export let month: number|string = new Date().getMonth()
-  export let from = ''
+  export let since = ''
   export let until = ''
-  export let wholeOption = ''
+  export let yearOption = ''
 
   const format = new Intl.DateTimeFormat(lang, {month: 'long'}).format
   export let months = [...Array(12).keys()].indexBy(m => m, m => format(new Date(2021, m)))
-  if (wholeOption) months['whole'] = wholeOption
+  if (yearOption) months['year'] = yearOption
+  setMonth(month as string)
 
   function setMonth(m: string) {
     let date = new Date()
-    date.setMonth(m == 'whole' ? 0 : +m)
     date.setDate(1)
-    from = date.toLocaleDateString('lt')
-    date.setMonth(m == 'whole' ? 12 : +m + 1)
+    date.setMonth(m == 'year' ? 0 : +m)
+    since = date.toLocaleDateString('lt')
+    date.setMonth(m == 'year' ? 12 : +m + 1)
     date.setDate(0)
     until = date.toLocaleDateString('lt')
   }
