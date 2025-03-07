c8a90c94b45ff533987f4ea4b0b6224d81ea1a02
Erkki Teedla <erkki@codeborne.com>
2025-03-07 08:42:24 +0200

    update checkbox component to svelte 5, also includes the possibility to render it as a switch. add also samples

---
+++ ui/src/forms/CheckboxField.samples.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import CheckboxField from './CheckboxField.svelte'
+  import type {ComponentProps} from 'svelte'
+</script>
+
+<h4>Default</h4>
+
+<div class="mb-4 grid w-64 gap-3">
+  {#each ["First check", "Second check", "Third check"] as item, index}
+    <CheckboxField label={item} checked={index === 0} disabled={index === 2}/>
+  {/each}
+</div>
+
+<h4>Render as toggle</h4>
+
+<div class="mb-4 grid w-64 gap-3">
+  {#each ["First switch", "Second switch", "Disabled switch"] as item, index}
+    <CheckboxField
+      label={item}
+      checked={index === 0}
+      disabled={index === 2}
+      renderAsToggle
+    />
+  {/each}
+</div>
+
+<h4>Standalone toggle</h4>
+<div class="text-sm">To be used with accompanied text</div>
+<div class="mb-4 grid w-64 gap-3">
+  <CheckboxField renderAsToggle/>
+  <CheckboxField checked={true} renderAsToggle/>
+</div>
--- ui/src/forms/CheckboxField.svelte
+++ ui/src/forms/CheckboxField.svelte
@@ -1,26 +1,89 @@
 <script lang="ts">
-  export let label: string|undefined = undefined
-  export let checked = false
+  import type { HTMLInputAttributes } from "svelte/elements";
 
-  export let id = label ? label.replace(/\./g, '-') : ''
+  interface Props extends HTMLInputAttributes {
+    label?: string;
+    checked?: boolean;
+    renderAsToggle?: boolean;
+  }
+
+  let {
+    label = "",
+    checked = $bindable(false),
+    renderAsToggle = false,
+    disabled,
+    ...restProps
+  }: Props = $props();
+
+  const id = label.replace(/\./g, "-");
 </script>
 
-<div class="relative flex items-start {$$props.class }">
-  <div class="flex items-center h-5">
-    <input {id} type="checkbox" bind:checked {...$$restProps}>
+<div class="{renderAsToggle ? 'toggle-switch' : ''} relative flex items-start {disabled ? 'disabled' : ''}">
+  <div class="check-container flex items-center">
+    <input {id} type="checkbox" bind:checked {...restProps} />
+    {#if renderAsToggle}
+      <div class="line"></div>
+      <div class="dot"></div>
+    {/if}
   </div>
   {#if label}
-    <div class="ml-3 text-sm">
-      <label for={id} class="text-gray-900 mb-0">{label}</label>
-    </div>
+    <label for={id} class="mb-0">{label}</label>
   {/if}
-  <slot/>
 </div>
 
-<style lang="postcss" global>
+<style lang="postcss">
   @reference "./../global.css";
 
-  input[type=checkbox] {
-    @apply focus:ring-primary-500 h-4 w-4 text-primary-500 border-gray-300 rounded;
+  input {
+    @apply h-4 w-4 rounded border-neutral-300 text-neutral-500 focus:border-neutral-500 focus:ring-neutral-500 disabled:border-neutral-200 disabled:bg-neutral-100;
+  }
+
+  label {
+    @apply ml-2 text-sm leading-tight;
+  }
+
+  .disabled label {
+    @apply opacity-50;
+  }
+
+  .toggle-switch {
+    @apply flex cursor-pointer items-center;
+  }
+
+  .toggle-switch .check-container {
+    @apply relative;
+  }
+
+  .toggle-switch.disabled .check-container {
+    @apply opacity-50;
+  }
+
+  .toggle-switch input {
+    @apply relative h-6 w-10 opacity-0;
+  }
+
+  .toggle-switch .dot,
+  .toggle-switch .line {
+    @apply pointer-events-none absolute;
+  }
+
+  .toggle-switch .dot {
+    @apply pointer-events-none top-1 left-1 h-4 w-4 rounded-full bg-white opacity-70 transition;
+  }
+
+  .toggle-switch .line {
+    @apply pointer-events-none left-0 h-6 w-10 rounded-full bg-neutral-600/50;
+  }
+
+  .toggle-switch input:checked ~ .dot {
+    @apply translate-x-full opacity-100;
+  }
+
+  .toggle-switch input:checked ~ .line {
+    @apply bg-success-500;
+  }
+
+  .toggle-switch input:focus ~ .line {
+    @apply ring-2 ring-neutral-500 ring-offset-2 outline-hidden;
   }
 </style>
