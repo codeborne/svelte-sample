70b6946308a7524d3576acfce04a625fc49c9c52
Erkki Teedla <erkki@codeborne.com>
2025-03-10 09:33:25 +0200

    improve the chart animations example

--- ui/src/slides/ui-animations/ExampleChartAnimation.svelte
+++ ui/src/slides/ui-animations/ExampleChartAnimation.svelte
@@ -1,38 +1,44 @@
 <script>
   import Tabs from 'src/components/Tabs.svelte'
+  import CheckboxField from 'src/forms/CheckboxField.svelte'
+
+  export let animationsEnabled = false
 
   export let tabComponents = ['2023', '2024']
   export let selectedTab = tabComponents[0]
 
   let chartData = [
-    {'month': 'Jan', 'coffee_2023': 45, 'coffee_2024': 50, 'productivity_2023': 60, 'productivity_2024': 65},
-    {'month': 'Feb', 'coffee_2023': 40, 'coffee_2024': 44, 'productivity_2023': 58, 'productivity_2024': 63},
-    {'month': 'Mar', 'coffee_2023': 50, 'coffee_2024': 55, 'productivity_2023': 70, 'productivity_2024': 78},
-    {'month': 'Apr', 'coffee_2023': 48, 'coffee_2024': 52, 'productivity_2023': 68, 'productivity_2024': 75},
-    {'month': 'May', 'coffee_2023': 55, 'coffee_2024': 60, 'productivity_2023': 72, 'productivity_2024': 80},
-    {'month': 'Jun', 'coffee_2023': 52, 'coffee_2024': 58, 'productivity_2023': 74, 'productivity_2024': 83},
-    {'month': 'Jul', 'coffee_2023': 30, 'coffee_2024': 35, 'productivity_2023': 40, 'productivity_2024': 45},
-    {'month': 'Aug', 'coffee_2023': 25, 'coffee_2024': 28, 'productivity_2023': 35, 'productivity_2024': 40},
-    {'month': 'Sep', 'coffee_2023': 50, 'coffee_2024': 55, 'productivity_2023': 72, 'productivity_2024': 78},
-    {'month': 'Oct', 'coffee_2023': 53, 'coffee_2024': 58, 'productivity_2023': 75, 'productivity_2024': 82},
-    {'month': 'Nov', 'coffee_2023': 45, 'coffee_2024': 50, 'productivity_2023': 65, 'productivity_2024': 72},
-    {'month': 'Dec', 'coffee_2023': 38, 'coffee_2024': 42, 'productivity_2023': 55, 'productivity_2024': 60}
+    { "month": "Jan", "coffee_2023": 45, "coffee_2024": 43, "productivity_2023": 62, "productivity_2024": 64 },
+    { "month": "Feb", "coffee_2023": 42, "coffee_2024": 44, "productivity_2023": 60, "productivity_2024": 58 },
+    { "month": "Mar", "coffee_2023": 50, "coffee_2024": 52, "productivity_2023": 72, "productivity_2024": 71 },
+    { "month": "Apr", "coffee_2023": 49, "coffee_2024": 48, "productivity_2023": 70, "productivity_2024": 68 },
+    { "month": "May", "coffee_2023": 54, "coffee_2024": 55, "productivity_2023": 74, "productivity_2024": 73 },
+    { "month": "Jun", "coffee_2023": 52, "coffee_2024": 50, "productivity_2023": 75, "productivity_2024": 76 },
+    { "month": "Jul", "coffee_2023": 30, "coffee_2024": 32, "productivity_2023": 42, "productivity_2024": 40 },
+    { "month": "Aug", "coffee_2023": 27, "coffee_2024": 25, "productivity_2023": 38, "productivity_2024": 37 },
+    { "month": "Sep", "coffee_2023": 52, "coffee_2024": 50, "productivity_2023": 74, "productivity_2024": 72 },
+    { "month": "Oct", "coffee_2023": 54, "coffee_2024": 53, "productivity_2023": 77, "productivity_2024": 75 },
+    { "month": "Nov", "coffee_2023": 46, "coffee_2024": 48, "productivity_2023": 67, "productivity_2024": 66 },
+    { "month": "Dec", "coffee_2023": 33, "coffee_2024": 41, "productivity_2023": 57, "productivity_2024": 56 }
   ]
 </script>
-<div class="absolute top-[5cqi] bottom-0 left-[5cqi] right-[5cqi] flex flex-col justify-between">
+<div class="absolute inset-[5cqi] flex flex-col justify-between">
   <div>
-    Toggle
+    <CheckboxField bind:checked={animationsEnabled} renderAsToggle label="Enable animations"/>
   </div>
 
-  <div class="font-semibold">Coffee Consumption per employee</div>
+  <div class="font-semibold">Avg. Coffee Consumption per Employee</div>
 
   <Tabs items={tabComponents} bind:activeTab={selectedTab} let:item />
 
 
   <div class="flex justify-between items-end gap-1 w-full flex-grow">
     {#each chartData as data}
-      <div class="col-span-1 bg-orange-900 rounded-t-xs w-full text-white text-xs p-1" style="height: {data['coffee_' + selectedTab]}%">
+      <div class="col-span-1 h-full flex flex-col justify-end">
+      <div class=" bg-orange-900 rounded-t-xs w-full text-white text-xs p-1 {animationsEnabled ? 'transition-all duration-700' : ''}" style="height: {data['coffee_' + selectedTab]}%">
         {data['coffee_' + selectedTab]}
+      </div>
+      <div class="text-[1.5cqi] text-center">{data.month}</div>
       </div>
     {/each}
   </div>
