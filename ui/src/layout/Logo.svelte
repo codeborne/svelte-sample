aece5f85e58185883073bb9dd9f646961b8e3551
Erkki Teedla <erkki@codeborne.com>
2025-05-13 13:04:54 +0300

    logo should also be usable without linking

--- ui/src/layout/Logo.svelte
+++ ui/src/layout/Logo.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   import {t} from 'src/i18n'
-  import Link from 'src/components/Link.svelte'
 </script>
 
-<Link to="">
-  <img class="h-7" src="/img/logo.png" title={t.title} alt="Codeborne logo">
-</Link>
+<img class="{$$props.class || 'h-7'}" src="/img/logo.png" title={t.title} alt="Codeborne logo">
