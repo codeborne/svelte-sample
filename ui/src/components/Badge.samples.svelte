8f51e779c909da2541b629362162e28bfb2d6a31
Erkki Teedla <erkki@codeborne.com>
2025-09-01 11:35:27 +0300

    add a more mature version of the badge component that supports sizing, variant and icons

--- ui/src/components/Badge.samples.svelte
+++ ui/src/components/Badge.samples.svelte
@@ -1,10 +1,73 @@
 <script lang="ts">
+  import type { ComponentProps } from "svelte";
   import Badge from 'src/components/Badge.svelte'
+  import Button from 'src/components/Button.svelte'
+
+
+  type BadgeSize = ComponentProps<typeof Badge>["size"];
+
+  const sizes: BadgeSize[] = ["xs", "sm", "", "lg"];
 </script>
 
-<div>
-  <Badge>Primary</Badge>
-  <Badge color="success">Success</Badge>
-  <Badge color="warning">Warning</Badge>
-  <Badge color="danger">Danger</Badge>
+<div class="space-y-6">
+  <h4 class="text-xl font-semibold">Regular badge</h4>
+
+  {#each sizes as size}
+    <div class="flex flex-col space-y-2">
+      <h5 class="text-xs uppercase">Size {size || "default"}</h5>
+      <div class="space-x-1">
+        <Badge {size}>Primary</Badge>
+        <Badge color="primary" {size}>Primary</Badge>
+        <Badge color="success" {size}>Success</Badge>
+        <Badge color="warning" {size}>Warning</Badge>
+        <Badge color="danger" {size}>Danger</Badge>
+      </div>
+    </div>
+  {/each}
+
+  <h4 class="text-xl font-semibold">Badge with icon</h4>
+
+  <p>
+    Badge can have icons to better illustrate its meaning, when the content might not be descriptive enough.
+    The icons can also improve intuitiveness especially with poor contrast situations or for people with color
+    blindness.
+  </p>
+
+  {#each sizes as size}
+    <div class="flex flex-col space-y-2">
+      <h5 class="text-xs uppercase">Size {size || "default"}</h5>
+      <div class="space-x-1">
+        <Badge {size} icon="user">John Doe</Badge>
+        <Badge {size} icon="check" color="success">Completed</Badge>
+        <Badge {size} class="flex-row-reverse" icon="x">Icon in the end</Badge>
+      </div>
+    </div>
+  {/each}
+
+  <h4 class="text-xl font-semibold">Numeric badge</h4>
+
+  {#each sizes as size}
+    <div class="flex flex-col space-y-2">
+      <h5 class="text-xs uppercase">Size {size || "default"}</h5>
+      <div class="flex items-center space-x-2">
+        <Button color="secondary" {size} class="gap-1">
+          <span class="text-nowrap">In a button</span>
+          <Badge variant="numeric" {size}>1</Badge>
+        </Button>
+        <Button color="secondary" {size} class="gap-1">
+          <span class="text-nowrap">In a button</span>
+          <Badge variant="numeric" color="warning" {size}>28</Badge>
+        </Button>
+        <Button color="secondary" {size} class="gap-1">
+          <span class="text-nowrap">In a button</span>
+          <Badge variant="numeric" color="success" {size}>888</Badge>
+        </Button>
+        <Button icon="notifications" {size} color="primary" class="circular relative">
+          <Badge variant="numeric" color="danger" class="absolute top-0 right-0 translate-x-1/3 -translate-y-1/3" {size}
+          >8</Badge
+          >
+        </Button>
+      </div>
+    </div>
+  {/each}
 </div>
--- ui/src/components/Badge.svelte
+++ ui/src/components/Badge.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
+  import Icon from 'src/icons/Icon.svelte'
 
   interface Props {
-    color?: "default" | "warning" | "success" | "danger";
+    color?: "default" | "primary" | "warning" | "success" | "danger";
     size?: "xs" | "sm" | "" | "lg";
+    variant?: "text" | "numeric";
+    class?: string;
+    icon?: string;
+    iconClass?: string;
     children: Snippet;
   }
 
-  const { color = "default", size = "", children }: Props = $props();
+  const { color = "default", size = "", variant = "text", class: className = "", icon, iconClass, children }: Props = $props();
 </script>
 
-<span class="badge {color} {size}">
+<span class="badge {color} {size} {variant} {icon ? 'has-icon' : ''} {className}">
+  {#if icon}
+    <Icon name={icon} {size} class={iconClass} />
+  {/if}
   {@render children()}
 </span>
 
@@ -18,30 +26,63 @@
   @reference "./../global.css";
 
   .badge {
-    @apply bg-primary-100 text-primary-950 inline-flex items-center rounded-full px-3 py-0.5 font-medium;
+    @apply inline-flex items-center align-middle rounded-full bg-neutral-200 px-2.5 py-0.5 font-medium text-neutral-800 dark:bg-neutral-800 dark:text-neutral-200;
   }
 
   .badge.xs {
-    @apply px-2 text-xs leading-tight;
+    @apply px-1.5 text-xs leading-tight;
   }
 
   .badge.sm {
-    @apply px-2.5 text-sm;
+    @apply px-2 text-sm;
   }
 
   .badge.lg {
-    @apply px-3.5 text-lg;
+    @apply px-3 text-lg leading-normal;
+  }
+
+  .badge :global(.icon) {
+    @apply -my-1 scale-75;
+  }
+
+  .badge.has-icon {
+    @apply inline-flex items-center gap-0.25;
+  }
+
+  .badge.has-icon:not(.flex-row-reverse) {
+    @apply ps-1;
+  }
+
+  .badge.flex-row-reverse.has-icon {
+    @apply pe-1;
+  }
+
+  .badge.primary {
+    @apply bg-primary-200 dark:bg-primary-800 dark:text-white;
   }
 
   .badge.success {
-    @apply bg-success text-primary-50;
+    @apply bg-success-200 dark:bg-success-800 dark:text-white;
   }
 
   .badge.danger {
-    @apply bg-danger text-primary-50;
+    @apply bg-danger-600 text-white;
   }
 
   .badge.warning {
-    @apply bg-warning text-primary-950;
+    @apply bg-warning-300 dark:bg-warning-500 text-warning-950;
+  }
+
+  .badge.numeric {
+    @apply flex aspect-auto items-center justify-center !px-1 !py-0 tracking-tighter tabular-nums;
+    min-width: 1.5em;
+  }
+
+  .badge.xs.numeric {
+    min-width: 1.25em;
+  }
+
+  .badge.sm.numeric {
+    min-width: 1.4em;
   }
 </style>
