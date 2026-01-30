0614996192faa4759341702f4c1cc0563340ccda
Anna Tomberg <anna@codeborne.com>
2026-01-30 12:08:36 +0200

    Add SampleSection

--- ui/src/components/Button.samples.svelte
+++ ui/src/components/Button.samples.svelte
@@ -1,48 +1,64 @@
 <script lang="ts">
   import Button from 'src/components/Button.svelte'
+  import SampleSection from 'src/layout/SampleSection.svelte'
 
-  const sizes = ['xs', 'sm', '', 'lg'] as const
+  const sizes = ['lg', '', 'sm', 'xs'] as const
   const colors = ['primary', 'secondary', 'danger', 'warning', 'success'] as const
   const variants = ['solid', 'soft', 'outlined', 'ghost'] as const
 
-  let isLoading = $state(false);
+  let isLoading = $state(false)
 </script>
 
-<div class="common-grid sm:grid-cols-4">
-  {#each sizes as size}
-    <div class="flex flex-col gap-6">
-      <h4>Size {size || 'default'}</h4>
-      {#each colors as color}
-        <div class="grid gap-2">
-          {#each variants as variant}
-            <Button label="{color} {variant}" {color} {size} {variant}/>
-          {/each}
-        </div>
-      {/each}
-      <Button label="With icon" {size} icon="link-external"/>
-      <Button label="Loading" {size} loading />
-      <Button
-        {size}
-        icon="chevron-right"
-        onclick={() => (isLoading = !isLoading)}
-        loading={isLoading}
-      >
-        {#if isLoading}
-          Loading
-        {:else}
-          Click to load
-        {/if}
-      </Button>
+<SampleSection title="Usage"
+               description="Use with props like color, size, variant, and icon. Buttons support loading states, custom click handlers, and can be circular for icon-only displays. Use the label prop or slot content for button text.">
+</SampleSection>
 
-      <div>
-        <Button {size} icon="copy" title="Icon only"/>
-      </div>
-
-      <div class="flex gap-2">
-        {#each variants as variant}
-          <Button icon="x" {size} {variant} circular title="Circular" />
+<SampleSection title="Sizes and variants">
+  <div class="common-grid sm:grid-cols-4">
+    {#each sizes as size}
+      <div class="flex flex-col gap-6">
+        <h4>Size {size || 'default'}</h4>
+        {#each colors as color}
+          <div class="grid gap-2">
+            {#each variants as variant}
+              <Button label="{color} {variant}" {color} {size} {variant}/>
+            {/each}
+          </div>
         {/each}
       </div>
-    </div>
-  {/each}
-</div>
+    {/each}
+  </div>
+</SampleSection>
+
+<SampleSection title="Icons and loading states">
+  <div class="common-grid sm:grid-cols-4">
+    {#each sizes as size}
+      <div class="flex flex-col gap-6">
+        <Button label="With icon" {size} icon="link-external"/>
+        <Button label="Loading" {size} loading/>
+        <Button
+          {size}
+          icon="chevron-right"
+          onclick={() => (isLoading = !isLoading)}
+          loading={isLoading}
+        >
+          {#if isLoading}
+            Loading
+          {:else}
+            Click to load
+          {/if}
+        </Button>
+
+        <div>
+          <Button {size} icon="copy" title="Icon only"/>
+        </div>
+
+        <div class="flex gap-2">
+          {#each variants as variant}
+            <Button icon="x" {size} {variant} circular title="Circular"/>
+          {/each}
+        </div>
+      </div>
+    {/each}
+  </div>
+</SampleSection>
