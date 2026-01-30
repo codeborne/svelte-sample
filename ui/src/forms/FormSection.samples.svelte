773ba09876ff2966f100f861fde5ab918ed6ac6a
Anna Tomberg <anna@codeborne.com>
2026-01-30 16:40:18 +0200

    Add SampleSection and card to illustrate text in "Usage"

--- ui/src/forms/FormSection.samples.svelte
+++ ui/src/forms/FormSection.samples.svelte
@@ -2,28 +2,37 @@
   import FormSection from 'src/forms/FormSection.svelte'
   import FormField from 'src/forms/FormField.svelte'
   import Button from 'src/components/Button.svelte'
+  import SampleSection from 'src/layout/SampleSection.svelte'
+  import Card from 'src/components/Card.svelte'
 </script>
 
-<p>Form section can be useful to be used in combination with cards.</p>
-<p>Using form section without a title and description can help aligning a more global submit button.</p>
 
-<FormSection title="Form section"
-             description="Here could be some helpful information about the form contents"
-             icon="info-circle"
->
-  <FormField label="Some field"/>
-  <FormField label="Some field"/>
-</FormSection>
+<SampleSection title="Usage">
+  <div>
+    <p>Form section can be useful to be used in combination with cards.</p>
+    <p class="mb-12">Using form section without a title and description can help aligning a more global submit
+      button.</p>
+  </div>
 
-<FormSection title="Another form section"
-             description="Here could be some helpful information about the form contents"
-             icon="alert-triangle"
->
-  <FormField label="Some field"/>
-  <FormField label="Some field"/>
-</FormSection>
+  <Card>
+    <FormSection title="Form section"
+                 description="Here could be some helpful information about the form contents"
+                 icon="info-circle"
+    >
+      <FormField label="Some field"/>
+      <FormField label="Some field"/>
+    </FormSection>
 
-<FormSection>
-  <Button label="Submit" class="success" size="lg"/>
-</FormSection>
+    <FormSection title="Another form section"
+                 description="Here could be some helpful information about the form contents"
+                 icon="alert-triangle"
+    >
+      <FormField label="Some field"/>
+      <FormField label="Some field"/>
+    </FormSection>
 
+    <FormSection>
+      <Button label="Submit" class="success" size="lg"/>
+    </FormSection>
+  </Card>
+</SampleSection>
