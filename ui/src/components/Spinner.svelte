15f9f0725e9b0f384dacb1c408eef1cb7f5976c2
Anna Tomberg <anna@codeborne.com>
2026-01-30 13:32:45 +0200

    Add spinner customization

--- ui/src/components/Spinner.svelte
+++ ui/src/components/Spinner.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
+  let { stroke, size, class: className, color }: { stroke?: string, size?: string, class?: string, color? : string} = $props()
 </script>
 
-<div class="spinner w-full text-center {$$props.class ?? 'h-11'}">
+<div class="spinner w-full text-center {className ?? 'h-11'}">
   <svg
-    class="animate-spin h-full"
+    class="animate-spin h-full {color ?? ''}"
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width={size ?? '24'}
+    height={size ?? '24'}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="1.5"
+    stroke-width={stroke ?? '1.5'}
     stroke-linecap="round"
     stroke-linejoin="round"
   >
