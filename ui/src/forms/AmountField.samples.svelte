87938a07d530926fde671c21fb0538524de0b436
Anna Tomberg <anna@codeborne.com>
2026-01-30 15:28:23 +0200

    Add SampleSection

--- ui/src/forms/AmountField.samples.svelte
+++ ui/src/forms/AmountField.samples.svelte
@@ -1,8 +1,12 @@
 <script>
   import AmountField from 'src/forms/AmountField.svelte'
+  import SampleSection from 'src/layout/SampleSection.svelte'
 </script>
 
-<div class="w-64 common-grid">
-  <AmountField label="Price" value={123.47}/>
-  <AmountField label="Price" value={1250} currency="PLN"/>
-</div>
+<SampleSection title="Usage"
+               description="Display monetary values with proper formatting.">
+  <div class="w-64 common-grid">
+    <AmountField label="Price" value={123.47}/>
+    <AmountField label="Price" value={1250} currency="PLN"/>
+  </div>
+</SampleSection>
--- ui/src/forms/CheckboxField.samples.svelte
+++ ui/src/forms/CheckboxField.samples.svelte
@@ -1,31 +1,37 @@
 <script lang="ts">
   import CheckboxField from './CheckboxField.svelte'
+  import SampleSection from 'src/layout/SampleSection.svelte'
 </script>
 
-<p>Choosing between default vs toggle – checkbox expects a save button, switch does not.</p>
+<SampleSection title="Usage"
+               description="Choosing between default vs toggle – checkbox expects a save button, switch does not.">
+</SampleSection>
 
-<h4>Default</h4>
+<SampleSection title="Default">
+  <div class="mb-4 grid w-64 gap-3">
+    {#each ["First check", "Second check", "Third check"] as item, index}
+      <CheckboxField label={item} checked={index === 0} disabled={index === 2}/>
+    {/each}
+  </div>
+</SampleSection>
 
-<div class="mb-4 grid w-64 gap-3">
-  {#each ["First check", "Second check", "Third check"] as item, index}
-    <CheckboxField label={item} checked={index === 0} disabled={index === 2}/>
-  {/each}
-</div>
+<SampleSection title="Toggle switches">
+  <div class="mb-4 grid w-64 gap-3">
+    {#each ["First switch", "Second switch", "Disabled switch"] as item, index}
+      <CheckboxField
+        label={item}
+        checked={index === 0}
+        disabled={index === 2}
+        renderAsToggle
+      />
+    {/each}
+  </div>
+</SampleSection>
 
-<div class="mb-4 grid w-64 gap-3">
-  {#each ["First switch", "Second switch", "Disabled switch"] as item, index}
-    <CheckboxField
-      label={item}
-      checked={index === 0}
-      disabled={index === 2}
-      renderAsToggle
-    />
-  {/each}
-</div>
-
-<h4>Standalone toggle</h4>
-<div class="text-sm">To be used with accompanied text</div>
-<div class="mb-4 grid w-64 gap-3">
-  <CheckboxField renderAsToggle/>
-  <CheckboxField checked={true} renderAsToggle/>
-</div>
+<SampleSection title="Standalone toggle">
+  <div class="text-sm">To be used with accompanied text</div>
+  <div class="mb-4 grid w-64 gap-3">
+    <CheckboxField renderAsToggle/>
+    <CheckboxField checked={true} renderAsToggle/>
+  </div>
+</SampleSection>
