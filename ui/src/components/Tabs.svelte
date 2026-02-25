a37ba81d12482acdfe7b8f765ad14b6b1428b420
Erkki Teedla <erkki@codeborne.com>
2026-02-25 15:56:11 +0200

    add tabs samples

--- ui/src/components/Tabs.svelte
+++ ui/src/components/Tabs.svelte
@@ -5,12 +5,12 @@
 </script>
 
 <div class="border-b border-gray-200 mb-8">
-  <nav class="-mb-px flex justify-end items-center space-x-8" aria-label="Tabs">
+  <nav class="-mb-px flex {$$props.class}" aria-label="Tabs">
     {#each items as item}
       <button onclick={() => activeTab = item} role="tab"
          class:border-primary-500={activeTab === item}
          class:border-transparent={activeTab !== item}
-         class="flex-1 text-gray-500 text-center hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
+         class="flex-1 text-gray-500 text-center hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-3 border-b-2 font-medium text-sm">
         {item}
       </button>
     {/each}
